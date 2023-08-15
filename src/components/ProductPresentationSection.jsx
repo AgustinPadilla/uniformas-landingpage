fca616cc3f9dc59140f2abe1d6dae227c5195f0b
Agustin Padilla <agustin.padilla@visitor.upm.com>
2023-08-15 17:59:01 -0300

    add react-reveal

--- src/components/ProductPresentationSection.jsx
+++ src/components/ProductPresentationSection.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Title } from './Title'
+import { Fade } from 'react-reveal'
 
 export const ProductPresentationSection = () => {
   const section = useRef(null)
@@ -34,22 +35,24 @@ export const ProductPresentationSection = () => {
 
   return (
     <section className='ColoredSection bg-sky-300' ref={section}>
+      <Fade>
 
-      <article className='flex flex-col max-w-2xl'>
-        <Title>Uniformes de calidad !</Title>
-        <h2 className='text-3xl font-extrabold text-sky-950 drop-shadow-lg mb-20'>Para escuelas, colegios y liceos</h2>
+        <article className='flex flex-col max-w-2xl'>
+          <Title>Uniformes de calidad !</Title>
+          <h2 className='text-3xl font-extrabold text-sky-950 drop-shadow-lg mb-20'>Para escuelas, colegios y liceos</h2>
 
-        <div className='text-xl font-medium drop-shadow-lg flex flex-col items-center max-w-xl gap-5'>
-          <p>Ofrecemos indumentaria del mejor nivel en una amplia variedad de estilos para niños y niñas. ¡Encuentra el estilo perfecto para tus hijos!</p>
-          <ul className='mt-10 w-full ml-52'>
-            <li className='mb-5 flex items-center'> <img src='nursery.svg' className='h-8 mr-4' />Jardineros personalizados</li>
-            <li className='mb-5 flex items-center'><img src='school.svg' className='h-8 mr-4' />Tunicas escolares</li>
-            <li className='mb-5 flex items-center'><img src='college.svg' className='h-8 mr-4' />Conjuntos de colegios y liceo</li>
-          </ul>
-          <button className='bg-sky-700 text-white py-2 px-7 shadow-xl hover:bg-sky-600 active:translate-y-1 active:bg-sky-800' ref={button}>Comprar</button>
-        </div>
+          <div className='text-xl font-medium drop-shadow-lg flex flex-col items-center max-w-xl gap-5'>
+            <p>Ofrecemos indumentaria del mejor nivel en una amplia variedad de estilos para niños y niñas. ¡Encuentra el estilo perfecto para tus hijos!</p>
+            <ul className='mt-10 w-full ml-52'>
+              <li className='mb-5 flex items-center'> <img src='nursery.svg' className='h-8 mr-4' />Jardineros personalizados</li>
+              <li className='mb-5 flex items-center'><img src='school.svg' className='h-8 mr-4' />Tunicas escolares</li>
+              <li className='mb-5 flex items-center'><img src='college.svg' className='h-8 mr-4' />Conjuntos de colegios y liceo</li>
+            </ul>
+            <button className='bg-sky-700 text-white py-2 px-7 shadow-xl hover:bg-sky-600 active:translate-y-1 active:bg-sky-800' ref={button}>Comprar</button>
+          </div>
 
-      </article>
+        </article>
+      </Fade>
 
       <picture className='w-96 flex flex-col'>
         <div className='flex flex-grow hover:animate-shake hover:animate-once hover:animate-duration-200 hover:animate-ease-in-out'>
