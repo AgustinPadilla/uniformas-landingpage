2102543defa3629efce54fbf471ebcc5a982bfde
Agustin Padilla <agustin0padilla2001@gmail.com>
2023-10-26 11:31:18 -0300

    fix padding, fix animation productcards

--- src/components/ProductsListSection.jsx
+++ src/components/ProductsListSection.jsx
@@ -8,13 +8,18 @@ export const ProductsListSection = forwardRef(function ProductsListSection (prop
     <section id='ProductListSection' className='h-fit ColoredSection bg-amber-300 flex-col items-center justify-start' ref={ref}>
       <Title>Revisa nuestros productos</Title>
 
-      <section className='w-4/5 flex flex-col md:flex-row gap-5'>
+      <section className='hidden md:flex w-4/5 md:flex-row gap-5'>
         <Zoom duration={500} cascade>
           <ProductCard img='jardinero.webp' title='Jardinero escolar' fabric='Zephyr' sizes='2 a 6' custom />
           <ProductCard img='tunica.jpg' title='Tunica blanca' fabric='Acrocel' sizes='6 a 14' custom />
           <ProductCard img='uniformeLiceo.jpeg' title='Equipo deportivo liceal' fabric='Dry Fit' sizes='8 a 16' custom />
         </Zoom>
+      </section>
 
+      <section className='w-4/5 flex flex-col md:hidden gap-5'>
+        <ProductCard img='jardinero.webp' title='Jardinero escolar' fabric='Zephyr' sizes='2 a 6' custom />
+        <ProductCard img='tunica.jpg' title='Tunica blanca' fabric='Acrocel' sizes='6 a 14' custom />
+        <ProductCard img='uniformeLiceo.jpeg' title='Equipo deportivo liceal' fabric='Dry Fit' sizes='8 a 16' custom />
       </section>
 
     </section>
