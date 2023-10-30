18471f71c385f0e99a0af11fd0a845ea60553497
Agustin Padilla <agustin0padilla2001@gmail.com>
2023-10-30 17:44:05 -0300

    fix context import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ContactProvider } from './context/Contact.jsx'
+import { ContactProvider } from './context/contact.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
